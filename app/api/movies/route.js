662fdf1b634cef3e0035c9d258c9728edb2a3b00
deesampan <76961599+deesampan@users.noreply.github.com>
2024-09-13 00:28:32 +0700

    idk

--- app/api/movies/route.js
+++ app/api/movies/route.js
@@ -3,8 +3,24 @@ import Topic from "@/models/topic";
 import { NextResponse } from "next/server";
 
 export async function POST(request){
-    const {movie_name,movie_url,movie_des} = await request.json();
+    const {movie_name,movie_url,movie_des,rank,movie_comment} = await request.json();
+    
+    console.log("this is rank : ", rank);
+
     await connectMongoDB();
-    await Topic.create({movie_name,movie_url,movie_des})
-    return NextResponse.json({message:"Topic Created"},{status:201});
+    await Topic.create({movie_name,movie_url,movie_des,rank,movie_comment})
+    return NextResponse.json({message:"Movie Created"},{status:201});
+}
+
+export async function GET(){
+    await connectMongoDB();
+    const topics = await Topic.find()
+    return NextResponse.json({topics})
+}
+
+export async function DELETE(request){
+    const id = request.nextUrl.searchParams.get("id");
+    await connectMongoDB();
+    await Topic.findByIdAndDelete(id);
+    return NextResponse.json({message: "Movie deleted"},{status:200})
 }
--- models/topic.js
+++ models/topic.js
@@ -1,16 +1,16 @@
-import mongoose,{Schema} from "mongoose";
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const topicSchema = new Schema(
-    {
-        movie_name: String,
-        movie_url : String,
-        movie_des: String,
-        rank : Number,
-    },{
-        timestamps:true,
-    }
-)
+  {
+    movie_name: String,
+    movie_url: String,
+    movie_des: String,
+    rank: Number,
+  }
+);
 
-const Topic = mongoose.model.Topic || mongoose.model("movie_lists",topicSchema);
+// Check if the model is already defined, and if not, define it
+const Topic = mongoose.models.movie_list || mongoose.model('movie_list', topicSchema);
 
-export default Topic;
+module.exports = Topic;
