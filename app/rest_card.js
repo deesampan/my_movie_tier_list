0b04179fc51a6b08995aa07d7f6d40904fb92fd3
deesampan <76961599+deesampan@users.noreply.github.com>
2024-09-13 23:32:36 +0700

    yes

---
+++ app/rest_card.js
@@ -0,0 +1,18 @@
+import styles from "./styles/restrank.module.css"
+import Image from "next/image";
+
+const RestCard = ({key,rank,movie_url,movie_name,movie_des}) =>{
+    return(
+        <div className={styles.content}>
+            <h1 className={styles.index}>#{rank}</h1>
+            <Image className={styles.image} src={movie_url} width={100} height={100} />
+            <h1 className={styles.movie_name}>{movie_name}</h1>
+            <h1 className={styles.des}>&quot;{movie_des}&quot;</h1>
+            <div className={styles.hr}>
+                <div className={styles.line}></div>
+            </div>
+        </div>
+    )
+}
+
+export default RestCard;
--- app/rest_rank.js
+++ app/rest_rank.js
@@ -1,21 +1,14 @@
 import styles from "./styles/restrank.module.css"
 import Image from "next/image";
+import RestCard from "./rest_card";
 
-const RestRank = ({key,data}) => {
+const RestRank = ({data}) => {
     const my_data = data.splice(3);
     return(
         <div className={styles.container}>
             {
                 my_data.map((t)=>(
-                    <div className={styles.content}>
-                        <h1 className={styles.index}>#{t.rank}</h1>
-                        <Image className={styles.image} src={t.movie_url} width={100} height={100} />
-                        <h1 className={styles.movie_name}>{t.movie_name}</h1>
-                        <h1 className={styles.des}>&quot;{t.movie_des}&quot;</h1>
-                        <div className={styles.hr}>
-                            <div className={styles.line}></div>
-                        </div>
-                    </div>
+                    <RestCard rank={t.rank} movie_url={t.movie_url} movie_name={t.movie_name} movie_des={t.movie_des} key={t._id}/>
                 ))
             }
             
