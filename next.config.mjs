0fefd106fa9b4141ef070b75eefd969eb9844302
deesampan <76961599+deesampan@users.noreply.github.com>
2024-09-14 01:14:24 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 const nextConfig = {
     images: {
       domains: ['i.ibb.co'], // Add the allowed external domains here
-    },
+    },eslint:{
+      ignoreDuringBuilds:true,
+    }
   };
   
   export default nextConfig; // Use export default for ES module syntax
